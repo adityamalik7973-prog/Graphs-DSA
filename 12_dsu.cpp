2c36a0da3ba5a97752c13d78e27e12deda1fde23
Aditya Malik <adityamalik7973@gmail.com>
2026-09-01 22:28:31 +0530

    DSU

---
+++ 12_dsu.cpp
@@ -0,0 +1,36 @@
+#include<bits/stdc++.h>
+using namespace std;
+class DisjointSetUnion{
+    int n;
+    vector<int> par,rank;
+public:
+    DisjointSetUnion(int n){
+        this->n=n;
+        for(int i=0;i<n;i++){
+            par.push_back(i);
+            rank.push_back(0);
+        }
+    }
+    int find(int x){
+        if(par[x]==x)return x;
+        return find(par[x]);
+    }
+    void DisjointSetUnite(int a,int b){
+        int parA=find(a);
+        int parB=find(b);
+        if(parA==parB)return;
+        if(rank[parA]==rank[parB]){
+            par[parB]=parA;
+            rank[parA]++;
+        }else if(rank[parA]>rank[parB]){
+            par[parB]=parA;
+        }else{
+            par[parA]=parB;
+        }
+    }
+    void getInfo(){
+        for(int i=0;i<n;i++)cout<<par[i]<<" ";
+        cout<<'\n';
+        for(int i=0;i<n;i++)cout<<rank[i]<<" ";
+    }
+};
